e950ecacbf42cbfee8bd2219139cea03f82e14ff
Carlos Bustamantem <cbustamantem@gmail.com>
2018-01-27 15:08:02 -0300

    release1

---
+++ src/main/java/py/org/jugpy/vtechelk/ElkLogger.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.org.jugpy.vtechelk;
+
+/**
+ *
+ * @author cbustamante
+ */
+public class ElkLogger {
+    public void init()
+    {
+//        RestHighLevelClient client = new RestHighLevelClient(
+//        RestClient.builder(
+//                new HttpHost("localhost", 9200, "http"),
+//                new HttpHost("localhost", 9201, "http")));
+    }
+    public static void main(String[] args) {
+       
+    }
+    
+}
